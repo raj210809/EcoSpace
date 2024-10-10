c4152162f1767fe51e2d41e2a10c96162638bd08
Dhanraj Kumar <dk7631333778@gmail.com>
2024-10-10 10:25:29 +0530

    adds chat feature for active

--- Econs-app-backend/index.js
+++ Econs-app-backend/index.js
@@ -1,10 +1,26 @@
 import express from "express"
+import {createServer} from 'http'
+import {Server} from 'socket.io'
 import { mongoconnect } from "./utils/connectdb.js"
 import authrouter from "./routers/authRoute.js"
 import dotenv from 'dotenv'
 dotenv.config({path:'./.env'})
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server , {
+    origin : '*',
+})
+
+io.on('connection' , (socket)=>{
+    console.log('a user connected')
+    socket.on("activemessage" , (msg)=>{
+        io.emit('group' , msg)
+    })
+    socket.on('disconnect', ()=>{
+        console.log('socket disconnected')
+    })
+})
 app.use(express.json())
 mongoconnect()
 
@@ -13,6 +29,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to the app!');
 });
 
-app.listen(3000 , ()=>{
+server.listen(3000 , ()=>{
     console.log('server is running on the port 3000')
 })
